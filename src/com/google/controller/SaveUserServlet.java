c03c13007bd27fdf978dc3f733b8012cfd08d52d
tejas <tejasshah@gmail.com>
2023-06-25 17:28:28 +0530

    read and print data

--- src/com/google/controller/SaveUserServlet.java
+++ src/com/google/controller/SaveUserServlet.java
@@ -1,16 +1,40 @@
 package com.google.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/SaveUserServlet")
-public class SaveUserServlet extends HttpServlet{
+public class SaveUserServlet extends HttpServlet {
 
-	public void service(HttpServletRequest request,HttpServletResponse response) {
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		System.out.println("SaveUserSerlvet Called...");
+
+		// read
+		String firstName = request.getParameter("firstName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		System.out.println(firstName);
+		System.out.println(email);
+		System.out.println(password);
+
+		// response print -> old approach => servlet -> generate html
+	
+		response.setContentType("text/html"); // MIME 
+		
+		PrintWriter  out = response.getWriter();  //server --> client 
+	
+		out.print("<html><body>");
+		
+		out.print("<br>FirstName => "+firstName);
+		out.print("<BR>Email => "+email);
+		out.print("<br>Password =>"+password);
+		
+		out.print("</body></html>");
 	}
 }
-
-
