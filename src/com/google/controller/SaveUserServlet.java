d51e60409254dd8921ce70a5f652e9f0e1ef9c47
tejas <tejasshah@gmail.com>
2023-07-01 13:22:25 +0530

    basic validation

--- src/com/google/controller/SaveUserServlet.java
+++ src/com/google/controller/SaveUserServlet.java
@@ -14,27 +14,47 @@ public class SaveUserServlet extends HttpServlet {
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		System.out.println("SaveUserSerlvet Called...");
 
-		// read
+		// read form data
 		String firstName = request.getParameter("firstName");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
+
+		boolean isError = false;
+		String error = "";
+		if (firstName == null || firstName.trim().length() == 0) {
+			isError = true;
+			error += "Please Enter FirstName<br>";
+		}
+
+		if (email == null || email.trim().length() == 0) {
+			isError = true;
+			error += "Please Enter Email<br>";
+		}
+
+		if (password == null || password.trim().length() == 0) {
+			isError = true;
+			error += "Please Enter Password<br>";
+		}
 
 		System.out.println(firstName);
 		System.out.println(email);
 		System.out.println(password);
 
 		// response print -> old approach => servlet -> generate html
-	
-		response.setContentType("text/html"); // MIME 
-		
-		PrintWriter  out = response.getWriter();  //server --> client 
-	
+
+		response.setContentType("text/html"); // MIME
+
+		PrintWriter out = response.getWriter(); // server --> client
+
 		out.print("<html><body>");
-		
-		out.print("<br>FirstName => "+firstName);
-		out.print("<BR>Email => "+email);
-		out.print("<br>Password =>"+password);
-		
+
+		if (isError == true) {
+			out.print(error);
+		} else {
+			out.print("<br>FirstName => " + firstName);
+			out.print("<BR>Email => " + email);
+			out.print("<br>Password =>" + password);
+		}
 		out.print("</body></html>");
 	}
 }
